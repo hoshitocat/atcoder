ae92687b4d3ffe0cc00b626cc3e4214eff6e054c
hoshitocat <hoshitocat@gmail.com>
2023-07-13 00:09:06 +0900

    :tada: answer shift only

--- shift_only.cpp
+++ shift_only.cpp
@@ -5,8 +5,32 @@ int main()
 {
   int n;
   cin >> n;
+
+  vector<int> vec(n); 
   for (int i = 0; i < n; i++) {
-    cout << i << "\n";
+    cin >> vec.at(i);
   }
+
+  bool oddFlag = false;
+  int cnt = 0;
+  while (true) {
+    for (int i = 0; i < n; i++) {
+      if (vec.at(i) % 2 == 0) {
+        vec.at(i) = vec.at(i) / 2;
+      } else {
+        oddFlag = true;
+        break;
+      }
+    }
+
+    if (!oddFlag) {
+      cnt++;
+    } else {
+      break;
+    }
+  }
+
+  cout << cnt << "\n";
+
   return 0;
 }
