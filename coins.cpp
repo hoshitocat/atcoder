c0c5cd9a974c2872a53a59d3d59d5b0ac5c83054
hoshitocat <hoshitocat@gmail.com>
2023-07-13 23:34:23 +0900

    :rocket: performance up coins

--- coins.cpp
+++ coins.cpp
@@ -1,70 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
-bool reachX(int x, int sum)
-{
-  if (sum == x) {
-    return true;
-  }
-  return false;
-}
-
-bool overX(int x, int sum)
-{
-  if (x < sum) {
-    return true;
-  }
-
-  return false;
-}
 
 int main()
 {
-  const int A = 500;
-  const int B = 100;
-  const int C = 50;
-  int a, b, c, x;
-  cin >> a;
-  cin >> b;
-  cin >> c;
-  cin >> x;
-
+  int A,B,C,X;
+  cin >> A >> B >> C >> X;
   int patterns = 0;
-  for (int i = 0; i <= a; i++) {
-    int sumA = A * i;
-    if (reachX(x, sumA)) {
-      patterns++;
-      continue;
-    }
-    if (overX(x, sumA)) {
-      break;
-    }
-
-    for (int j = 0; j <= b; j++) {
-      int sumB = j * B;
-      if (reachX(x, sumA + sumB)) {
-        patterns++;
-        continue;
-      }
-      if (overX(x, sumA + sumB)) {
-        break;
-      }
-
-      for (int k = 0; k <= c; k++) {
-        int sumC = k * C;
-        if (reachX(x, sumA + sumB + sumC)) {
-          patterns++;
-          continue;
-        }
-        if (overX(x, sumA + sumB + sumC)) {
-          break;
-        }
+  for (int a = 0; a <= A; a++) {
+    for (int b = 0; b <= B; b++) {
+      for (int c = 0; c <= C; c++) {
+        int sum = 500*a + 100*b + 50*c;
+        if (sum == X) patterns++;
       }
     }
   }
 
-  cout << patterns;
-
-  return 0;
+  cout << patterns << endl;
 }
