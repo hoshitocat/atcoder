a420864c3965d82d163e0dc6a6b289b1eefed115
hoshitocat <hoshitocat@gmail.com>
2023-07-14 23:10:40 +0900

    :tada: clear ABC085B - Kagami Mochi

---
+++ contests/abs/tasks/abc085_b/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  vector<int> vec(N);
+  for (int i = 0; i < N; i++) {
+    cin >> vec.at(i);
+  }
+
+  sort(vec.begin(), vec.end(), greater<int>());
+
+  if (N == 1) return 1;
+
+  int layers = 1;
+  for (int i = 1; i < N; i++) {
+    if (vec.at(i-1) == vec.at(i)) continue;
+    layers++;
+  }
+
+  cout << layers << endl;
+}
