efd49a58be96445caec167c52757ffa56924eef7
hoshitocat <hoshitocat@gmail.com>
2023-07-17 22:34:03 +0900

    :tada: clear 2023/07/17

--- contests/abs/tasks/abc085_c/main.cpp
+++ contests/abs/tasks/abc085_c/main.cpp
@@ -8,9 +8,20 @@ int main()
   int Y = 0;
   cin >> N >> Y;
 
-  int x = 0;
-  int y = 0;
-  int z = 0;
+  const int x = 10000;
+  const int y = 5000;
+  const int z = 1000;
 
-  cout << N << endl;
+  for (int a = 0; a*x <= Y; a++) {
+    for (int b = 0; b*y <= Y; b++) {
+      for (int c= 0; c*z <= Y; c++) {
+        if ((Y == (a*x + b*y + c*z)) && (N == a+b+c)) {
+          cout << a << " " << b << " " << c << endl;
+          return 0;
+        }
+      }
+    }
+  }
+
+  cout << -1 << " " << -1 << " " << -1 << endl;
 }
