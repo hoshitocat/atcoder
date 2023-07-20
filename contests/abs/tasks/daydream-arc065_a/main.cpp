75ac77cb68473c9a6d91361c900b6a409155e731
hoshitocat <hoshitocat@gmail.com>
2023-07-20 23:20:48 +0900

    :bug: daydream MLE

--- contests/abs/tasks/daydream-arc065_a/main.cpp
+++ contests/abs/tasks/daydream-arc065_a/main.cpp
@@ -2,29 +2,37 @@
 
 using namespace std;
 
+string words[4] = {"dreamer", "eraser", "dream", "erase"};
+
+bool find(string* str)
+{
+  if (str->size() == 0) {
+    return true;
+  }
+
+  if (str->size() < 5) {
+    return false;
+  }
+
+  for (int i = 0; i < 4; i++) {
+    string s = words[i];
+    if (str->substr(0, s.size()) == s) {
+      string remain = str->substr(s.size(), str->size());
+      if (find(&remain)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 int main()
 {
   string str;
   cin >> str;
 
-  string words[4] = {"dreamer", "eraser", "dream", "erase"};
-
-  bool flag = false;
-  while (5 <= str.size()) {
-    flag = false;
-    for (int i = 0; i < 4; i++) {
-      string s = words[i];
-      if (str.substr(0, s.size()) == s) {
-        str = str.substr(s.size(), str.size());
-        flag = true;
-        break;
-      }
-    }
-
-    if (!flag) {
-      break;
-    }
-  }
+  bool flag = find(&str);
 
   if (flag) {
     cout <<  "YES" << endl;
