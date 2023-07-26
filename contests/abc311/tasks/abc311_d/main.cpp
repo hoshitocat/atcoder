3c1e75ce8f07fb987affb9a2cd90bb52308d7e35
hoshitocat <hoshitocat@gmail.com>
2023-07-26 23:08:29 +0900

    :wip: 2023/07/26

--- contests/abc311/tasks/abc311_d/main.cpp
+++ contests/abc311/tasks/abc311_d/main.cpp
@@ -1,59 +1,25 @@
 // https://atcoder.jp/contests/abc311/tasks/abc311_d
-#include <iostream>
-#include <vector>
-#include <set>
-#include <queue>
+#include<iostream>
+#include<queue>
+#include<set>
 
 using namespace std;
 
-int n,m;
-vector<string> grid;
-set<pair<int,int>> seen;
+int main()
+{
+  int N, M; cin >> N >> M;
+  string grid[N];
+  for (int i = 0; i < N; i++) {
+    cin >> grid[i];
+  }
 
-int main() {
-    vector<pair<int,int>> moves {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    cin>>n>>m;
-    for (int i = 0; i < n ;i ++) {
-        string s;
-        cin>>s;
-        grid.push_back(s);
-    }
+  queue<pair<int, int>> q;
+  q.push({1,1});
 
-    queue<pair<int,int>> pq;
-    pq.push({1,1});
+  set<pair<int, int>> seen;
 
-    while(!pq.empty()) {
-        pair<int,int> x = pq.front();
-        pq.pop();
-
-        if (seen.find(x) != seen.end()) continue;
-        seen.insert(x);
-        // cout << x.first << " " << x.second << endl;
-
-
-        // left right up down
-        for (int i = 0; i < 4; i++) {
-            auto move = moves[i];
-            auto start = x;
-            while (grid[start.first][start.second] != '#') {
-                grid[start.first][start.second] = '1';
-                start.first += move.first;
-                start.second += move.second;
-            }
-            start.first -=move.first;
-            start.second -= move.second;
-            pq.push(start);
-        }
-    }
-    int result = 0;
-    for (auto x : grid) {
-        for (auto y : x) {
-            if (y == '1') {
-                result +=1;
-            }
-        }
-    }
-    cout <<result << endl;
-
-    return 0;
+  cout << q.front().first << endl;
+  while (!q.empty()) {
+    pair<int, int> p = q.pop();
+  }
 }
