a54d866948ab8eab122c9bf4f5cf96409d8d54f4
hoshitocat <hoshitocat@gmail.com>
2023-07-28 09:56:05 +0900

    :wip: 2023/07/28

--- contests/abc311/tasks/abc311_d/main.cpp
+++ contests/abc311/tasks/abc311_d/main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 int main()
 {
+  // left, right, down, up
+  vector<pair<int, int>> moves {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
   int N, M; cin >> N >> M;
   string grid[N];
   for (int i = 0; i < N; i++) {
@@ -20,12 +22,33 @@ int main()
 
   cout << q.front().first << endl;
   while (!q.empty()) {
-    pair<int, int> p = q.pop();
+    pair<int, int> p = q.front();
+    q.pop();
 
     if (seen.find(p) != seen.end()) continue;
     seen.insert(p);
 
     for (int i = 0; i < 4; i++) {
+      pair<int, int> move = moves[i];
+      pair<int, int> start = p;
+      while (grid[start.first][start.second] != '#') {
+        grid[start.first][start.second] = '1';
+        start.first += move.first;
+        start.second += move.second;
+      }
+      start.first -= move.first;
+      start.second -= move.second;
+      q.push(start);
     } 
   }
+
+  int result = 0;
+  for (string x : grid) {
+    for (char y : x) {
+      if (y == '1') result += 1;
+    }
+  }
+
+  cout << result << endl;
+  return 0;
 }
