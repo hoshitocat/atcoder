15ad2cdf19cddfc0eda4866053c9a0612c73e5d1
hoshitocat <hoshitocat@gmail.com>
2023-08-01 23:37:42 +0900

    :wip: 2023/08/01

--- contests/abc311/tasks/abc311_e/main.cpp
+++ contests/abc311/tasks/abc311_e/main.cpp
@@ -13,6 +13,11 @@ int main()
     }
   }
 
+  for (int n = 0; n < N; n++) {
+    int x, y; cin >> x >> y;
+    grid[x-1][y-1] = 1;
+  }
+
   for (int x = 0; x < X; x++) {
     for (int y = 0; y < Y; y++) {
       cout << grid[x][y] << endl;
