8765bee7846de0ecd4f7993ecfffe0a0e205724f
hoshitocat <hoshitocat@gmail.com>
2023-08-08 09:50:20 +0900

    :construction: init beauty grid

--- contests/abc311/tasks/abc311_f/main.cpp
+++ contests/abc311/tasks/abc311_f/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
   int N, M; cin >> N >> M;
+  vector<string> s(N);
+  for (auto &ss : s) {
+    cin >> ss;
+  }
+
   for (int n = 0; n < N; n++) {
-    string s;
-    cin >> s;
-    cout << s << endl;
+    for (int m = 0; m < M; m++) {
+      if (s[n][m] == '#') {
+        if (n+1 < N)
+          s[n+1][m] = '#';
+        if (n+1 < N && m+1 < M)
+          s[n+1][m+1] = '#';
+      }
+    }
+  }
+
+  // TODO
+  for (int n = 0; n < N; n++) {
+    cout << s[n] << endl;
   }
 }
